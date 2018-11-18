0defe6a0b8db70aa0cc9c9c379a022b56a16df59
toddlerya <toddlerya@gmail.com>
2018-11-18 23:58:19 +0800

    调整目录结构

--- verify_img_code/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-# @Time     : 2018/11/14 11:09  
-# @Author   : toddlerya
-# @FileName : __init__.py.py
-# @Project  : test
-
-from __future__ import unicode_literals
